238b84bf26f184f29369d7fb947bf708ab207431
HeatLai <hitgunlai@gmail.com>
2023-01-19 12:44:45 +0800

    DisallowHashCommentsSniff

---
+++ HeatlaiCodingStandard/Sniffs/Commenting/DisallowHashCommentsSniff.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HeatlaiCodingStandard\Sniffs\Commenting;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class DisallowHashCommentsSniff implements Sniff
+{
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return [T_COMMENT];
+    }
+
+    /**
+     * Processes this sniff, when one of its tokens is encountered.
+     *
+     * @param  \PHP_CodeSniffer\Files\File  $phpcsFile  The current file being checked.
+     * @param  int  $stackPtr  The position of the current token in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $comment = trim($tokens[$stackPtr]['content']);
+        if ($comment[0] === '#') {
+            $error = 'Hash comments are prohibited; found "%s"';
+            $data = [$comment];
+            $phpcsFile->addError($error, $stackPtr, 'Found', $data);
+        }
+    }
+}
