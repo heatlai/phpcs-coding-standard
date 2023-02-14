627b7c5f678105eba2b84873779a29ffa8b22c45
HeatLai <hitgunlai@gmail.com>
2023-02-14 16:22:25 +0800

    refactor: FuelPhpStyleClosureOpeningBrace

--- HeatlaiCodingStandard/Sniffs/Functions/FuelPhpStyleClosureOpeningBraceSniff.php
+++ HeatlaiCodingStandard/Sniffs/Functions/FuelPhpStyleClosureOpeningBraceSniff.php
@@ -5,8 +5,7 @@ namespace HeatlaiCodingStandard\Sniffs\Functions;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\Functions\OpeningFunctionBraceBsdAllmanSniff;
-use PHP_CodeSniffer\Util\Tokens;
-use HeatlaiCodingStandard\Helpers\FunctionDeclaration;
+use PHP_CodeSniffer\Standards\Squiz\Sniffs\Functions\MultiLineFunctionDeclarationSniff;
 
 class FuelPhpStyleClosureOpeningBraceSniff implements Sniff
 {
@@ -21,11 +20,18 @@ class FuelPhpStyleClosureOpeningBraceSniff implements Sniff
     {
         $tokens = $phpcsFile->getTokens();
 
-        if (isset($tokens[$stackPtr]['scope_opener'], $tokens[$stackPtr]['scope_closer']) === false) {
+        if (
+            isset($tokens[$stackPtr]['scope_opener']) === false
+            || isset($tokens[$stackPtr]['scope_closer']) === false
+            || isset($tokens[$stackPtr]['parenthesis_opener']) === false
+            || isset($tokens[$stackPtr]['parenthesis_closer']) === false
+        ) {
             return;
         }
 
-        if (FunctionDeclaration::isMultiLineDeclaration($phpcsFile, $stackPtr) === false) {
+        $multiLineSniff = new MultiLineFunctionDeclarationSniff();
+        $openBracket = $tokens[$stackPtr]['parenthesis_opener'];
+        if ($multiLineSniff->isMultiLineDeclaration($phpcsFile, $stackPtr, $openBracket, $tokens) === false) {
             $sniff = new OpeningFunctionBraceBsdAllmanSniff();
             $sniff->checkClosures = true;
             $sniff->process($phpcsFile, $stackPtr);
